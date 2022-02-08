11556558ba035c4e5c75fe692cbd58cd2f7dcb4d
Bob Merullo <bmerullo85@gmail.com>
2022-02-08 12:42:14 -0700

    App.js/ App.css/ index.html commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { Router } from '@reach/router'
+import NavBar from './components/NavBar';
+import react, { useState } from 'react'
+import BoxView from './views/BoxView';
+import TabView from './views/TabView';
+import ListView from './views/ListView';
+
+
 
 function App() {
+
+  
+
+
+
+
   return (
     <div className="App">
-      
+      <h1>Simple Projects</h1>
+      <NavBar />
+      <Router>
+        <BoxView path="/projects/box"/>
+        <TabView path="/projects/tab"/>
+        <ListView path="/projects/list"/>
+      </Router>
     </div>
   );
 }
